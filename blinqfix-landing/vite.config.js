7589957e0c72be63efd54eafe03879f6449663d6
Douglas Montas <douglasmontas@Douglass-MacBook-Pro.local>
2025-06-21 15:01:29 -0400

    added redirects

--- blinqfix-landing/vite.config.js
+++ blinqfix-landing/vite.config.js
@@ -1,12 +1,10 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({ plugins: [react()] });
 // vite.config.js
-export default {
-    root: './src', // optional if using src as source
-    build: {
-      outDir: 'dist',
-    },
-  };
-  
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
+});

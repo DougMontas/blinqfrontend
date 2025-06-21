2a45b9a3f87e95dce287d970cd2b5eabfb30fe04
Douglas Montas <douglasmontas@Douglass-MacBook-Pro.local>
2025-06-21 12:05:10 -0400

    added redirects

--- blinqfix-landing/src/App.jsx
+++ blinqfix-landing/src/App.jsx
@@ -1,19 +1,23 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Landing from './Landing';
-import CustomerHome from './pages/CustomerHome';
-import ProHome from './pages/ProHome';
-import ProviderNetwork from './pages/ProviderNetwork';
-import Customers from './pages/Customers';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Landing from "./Landing";
+import CustomerHome from "./pages/CustomerHome";
+import ProHome from "./pages/ProHome";
+import ProviderNetwork from "./pages/ProviderNetwork";
+import Customers from "./pages/Customers";
+import SuccessRedirect from "./pages/onboarding-success";
+import FailedRedirect from "./pages/onboarding-failed";
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <Routes>
-      <Route path="/"        element={<Landing />} />
+      <Route path="/" element={<Landing />} />
       <Route path="/customer" element={<CustomerHome />} />
-      <Route path="/pro"      element={<ProHome />} />
+      <Route path="/pro" element={<ProHome />} />
       <Route path="/customers" element={<Customers />} />
-      <Route path="/pros"      element={<ProviderNetwork />} />
-      <Route path="*" element={<Navigate to="/" replace/>} />
+      <Route path="/pros" element={<ProviderNetwork />} />
+      <Route path="/onboarding-success" element={<SuccessRedirect />} />
+      <Route path="/onboarding-failed" element={<FailedRedirect />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }

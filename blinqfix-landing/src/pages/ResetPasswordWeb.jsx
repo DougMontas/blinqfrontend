89772f574be198366b04c4b682e2507f2a82cefb
Douglas Montas <douglasmontas@Douglass-MacBook-Pro.local>
2025-07-27 01:31:15 -0400

    password reset

--- blinqfix-landing/src/pages/ResetPasswordWeb.jsx
+++ blinqfix-landing/src/pages/ResetPasswordWeb.jsx
@@ -1,3 +1,19 @@
+// import { useParams } from "react-router-dom";
+// import { useEffect } from "react";
+
+// export default function ResetPasswordWeb() {
+//   const { token } = useParams();
+
+//   useEffect(() => {
+//     // Redirect to app with deep link
+//     // window.location.href = `blinqfix://reset-password/${token}`;
+//     window.location.href = `blinqfix://ResetPasswordScreen?token=${token}`
+//   }, [token]);
+
+//   return <p>Redirecting you to the BlinqFix app...</p>;
+// }
+
+
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 
@@ -5,11 +21,14 @@ export default function ResetPasswordWeb() {
   const { token } = useParams();
 
   useEffect(() => {
-    // Redirect to app with deep link
-    // window.location.href = `blinqfix://reset-password/${token}`;
-    window.location.href = `blinqfix://ResetPasswordScreen?token=${token}`
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      window.location.href = `blinqfix://ResetPasswordScreen?token=${token}`;
+    } else {
+      // Optionally redirect to a helpful page
+      window.location.href = `https://blinqfix.com/download`; // or home
+    }
   }, [token]);
 
   return <p>Redirecting you to the BlinqFix app...</p>;
 }
-
